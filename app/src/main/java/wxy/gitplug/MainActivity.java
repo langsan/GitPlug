25fda79f55a8c026a12cb000a7bb69a3ec3ea53f
373138483 <373138483@qq.com>
2016-09-03 13:03:13 +0800

    测试22

--- app/src/main/java/wxy/gitplug/MainActivity.java
+++ app/src/main/java/wxy/gitplug/MainActivity.java
@@ -1,13 +1,26 @@
 package wxy.gitplug;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+
+//        TextView tv=new TextView(this);
+//        tv.setText("Hello World....");
+//        setContentView(tv);
+
+//        View view = LayoutInflater.from(MainActivity.this).inflate(R.layout.activity_main, null);
+//
+//        setContentView(view);
+
+        View view = View.inflate(MainActivity.this, R.layout.activity_main, null);
+
+        setContentView(view);
     }
 }
